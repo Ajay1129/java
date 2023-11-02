aeb2fe3b2ad45dff8f43c4a1feb59873a2617dd4
Ajay1129 <ajaymudhiraj2911@gmail.com>
2023-11-02 13:14:12 +0530

    my first java

---
+++ profit.java
@@ -0,0 +1,18 @@
+class profitloss
+{
+public static void main(String args[])
+ {
+  String pname="laptop";
+  int actualprice=90000;
+  int sellingprice=50000;
+  int total=actualprice-sellingprice;
+  if(actualprice>sellingprice)
+    {
+     System.out.println("loss");
+   }
+   else
+   { 
+     System.out.println("profit");
+   }
+  }
+}
---
+++ program.java
@@ -0,0 +1,24 @@
+class program
+   {
+    public static void main(String args[])
+    {
+      String name="tej";
+      int age=24;
+      String gender="male";
+      if(age>=25||gender=="female")
+         {
+           System.out.println("elgible to program");
+          }
+          else
+          {
+         System.out.println("not elgible");
+      }
+} 
+}
+          
+     
+         
+
+      
+        
+        
