a6bb42dfce4bfdda99717e65fb589f94f03060e7
Ajay1129 <ajaymudhiraj2911@gmail.com>
2023-11-03 12:46:31 +0530

    dwitej

---
+++ perfectsquare.java
@@ -0,0 +1,13 @@
+class Perfectsquare
+{
+   public static void main(String args[])
+   {
+   int num=31;
+   double num1=Math.sqrt(num);
+   if(num1==(int)num1)
+   {
+   System.out.print("perfectsquare");
+   }
+   }
+  
+}
